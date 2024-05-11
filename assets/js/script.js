83893fe13f7df90d73d4a59e1ca54131f00b6d4f
aniketrajpure <117091540+aniketrajpure@users.noreply.github.com>
2024-05-11 09:01:34 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,7 +59,7 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Portfolio | Jigar Sable";
+            document.title = "Portfolio | Aniket Rajpure";
             $("#favicon").attr("href", "assets/images/favicon.png");
         }
         else {
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "backend development", "web designing", "android development", "web development"],
+    strings: ["AI Enthusiast", "Frontend development", "backend development", "web development"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
